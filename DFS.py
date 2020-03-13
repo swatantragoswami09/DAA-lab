6f4c767f80a4a32cc5dcf737f1c8c8b0a317813e
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-03-13 20:19:42 +0530

    Create DFS.py

---
+++ DFS.py
@@ -0,0 +1,21 @@
+def dfs_iterative(graph, start):
+    queue = [start] #start
+    path = [] #empty
+    # print(stack[0])
+
+    while queue: #stack is not empty
+        vertex = queue.pop(0) #get first node to vertex 
+        if vertex in path: #node already in path
+            continue #skip
+        path.append(vertex) #start adding node to path
+        for neighbor in graph[vertex]: #start exploring other node
+            queue.append(neighbor) #add neighbor node to stack
+
+    return path
+
+
+adjacency_matrix = {'A': ['B', 'C','D'], 'B': ['E','F'],'C':['G','H'],'D':['I'],'E':['j','k'],'F':[],
+                    'G':['L'], 'H': [], 'I': ['M'],
+                    'j': [], 'k': ['N'],'L':[],'M':[],'N':[]}
+
+print(dfs_iterative(adjacency_matrix,'A'))
